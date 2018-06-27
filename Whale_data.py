d9c950bef2ca7ebfb4afc0ea952bab6259e7b104
lazartea <lazartea@reed.edu>
2018-06-27 16:53:26 -0700

    make functions more readable, better var names

--- Whale_data.py
+++ Whale_data.py
@@ -1,56 +1,54 @@
 import networkx as nx
 import graphspace_utils
-
 from operator import itemgetter
 import matplotlib.pyplot as plt
 import math
-G = nx.read_gml("mw_thesisgraph.gml",) #this reads Michael's graph into a useable networkx format
 
+#this reads Michael's graph into a useable networkx format
+#Graph = nx.read_gml("mw_thesisgraph.gml",) 
 
-d = {}
-for node in G.nodes():
-  for attr in G.nodes(node):
-    d[str(attr[0])] = attr[1]
-print(G.nodes())
+def createAttrDictionary(Graph):
+  attr_dictionary = {}
+  for node in Graph.nodes():
+    for attr in Graph.nodes(node):
+      attr_dictionary[str(attr[0])] = attr[1]
+  print(G.nodes())
+  return attr_dictionary
 
-#these plots were used to produce scatterplots of the baseline centrality measures
+def initClosenessCetrality(closenessCentrality):
+  close_node,close_val,close_label = ([] for i in range(3))
+  
+  num = 1
+  for key in closenessCentrality:
+    close_node.append(num)
+    close_label.append(str(key))
+    close_val.append(close[key])
+    num +=1
 
-close = nx.closeness_centrality(G,u=None,distance=None,normalized=True)
-close_node = []
-close_val = []
-close_label = []
-degree = nx.degree_centrality(G)
-degree_node = []
-degree_val = []
-degree_label = []
-between = nx.betweenness_centrality(G,k=None,normalized=True,weight=None,endpoints=False,seed=None)
-between_node = []
-between_val = []
-between_label = []
+  return [close_node,close_label,close_val]
 
+def initDegreeCentrality(degreeCentrality):
+  degree_node,degree_val,degree_label = ([] for i in range(3))
 
-i = 1
-for key in close:
-  close_node.append(i)
-  close_label.append(str(key))
-  close_val.append(close[key])
-  i += 1
-i = 1
-for key in degree:
-  degree_node.append(i)
-  degree_label.append(str(key))
-  degree_val.append(degree[key])
-  i += 1
-i = 1
-for key in between:
-  between_label.append(str(key))
-  between_node.append(i)
-  between_val.append(between[key])
-  i += 1
-i = 1
+  num = 1
+  for key in degreeCentrality:
+    degree_node.append(degree_num)
+    degree_label.append(str(key))
+    degree_val.append(degree[key])
+    num += 1
 
+  return [degree_node,degree_label,degree_val]
 
+def initBetweenessCentrality(betweennessCentrality):
+  between_node,between_val,between_label = ([] for i in range(3))
 
+  num = 1
+  for key in betweennessCentrality:
+    between_node.append(num)  
+    between_label.append(str(key))
+    between_val.append(between[key])
+    num += 1
+  return [between_node,between_label,between_val]
 
 def plot(prefix,x,y,labels):
   fig = plt.figure(figsize=(6.5,4))
@@ -59,108 +57,86 @@ def plot(prefix,x,y,labels):
   plt.ylabel(prefix)
   plt.title(prefix)
   plt.subplots_adjust(bottom=0.15)
-
   plt.tight_layout()
-
   plt.savefig(prefix+'.png')
   print "Wrote to "+prefix+".png"
   return 
-  
-#plot("In Degreee centrality",in_deg_node,in_deg_val,in_deg_label)
 
-def centrality(G): #input: a networkx graph G
-                   #output:
-  degree = nx.degree_centrality(G)
-  
-  
+def centrality(G):
   close = nx.closeness_centrality(G,u=None,distance=None,normalized=True)
+  degree = nx.degree_centrality(G)
   between = nx.betweenness_centrality(G,k=None,normalized=True,weight=None,endpoints=False,seed=None)
-  
-  return [degree,close,between]
 
+  return [closenessCentrality,degreeCentrality,betweennessCentrality]
 
-def NodeRemoval(G): #input: a networkx graph G
-                    #output: a list of dictionaries for three centrality measures of each graph that has been created through removing indiv. nodes
-  listCent = {}
-  for n in G.nodes(): #iterate through each node in the graph
+#input: a networkx graph, G
+#output: a list of dictionaries for three centrality measures of each graph that
+#has been created through removing individual nodes
+def NodeRemoval(Graph): 
+  centralityDictionaries = {}
+  for node in Graph.nodes():
     removedEdge = []
-    G.remove_node(n) #remove node of interest
-    for edge in G.edges():
+    Graph.remove_node(node)
+    for edge in Graph.edges():
       if node in edge:
         removedEdge.append(edge)
-        G.remove_edge(edge[0],edge[1]) #remove edges with the node of interest
-    listCent[n] = centrality(G) #computes centrality measures with node removed, adds to a dictionary
-    G.add_node(n) #add node back to Graph
+        Graph.remove_edge(edge[0],edge[1])
+    centralityDictionaries[node] = centrality(Graph)
+    Graph.add_node(node)
     for edge in removedEdge:
-      G.add_edge(edge[0],edge[1]) #add removed edges back to graph
+      Graph.add_edge(edge[0],edge[1])
 
-  for key in listCent:
-    print key,listCent[key]
-    print
-    print
-  return listCent #return dictionary 
-y = centrality(G)
-x = NodeRemoval(G)
-
-def Comparison(G_Cent,ind_cent,per): #input: baseline centrality measures for a graph, centrality measures for each version of the graph with a node removed, list of baseline centrality measures means
-                                 #output: a list of nodes in order of the change that it's removal caused
+  for key in centralityDictionaries:
+    print key,centralityDictionaries[key] + "\n \n"
+  return centralityDictionaries
   
-  full_ls = []
-  for key in ind_cent:
-    for item in range(3):
+#input: baseline centrality measures for a graph, centrality measures for each version
+# of the graph with a node removed, list of baseline centrality measures means
+#output: a list of nodes in order of the change that it's removal caused
+def Comparison(graphCentrality,individualCentrality,baseline):  
+  influentialNodeList = []
+  for key in individualCentrality:
+    for measure in range(3):
       change = {}
-      x = ind_cent[key][item] 
+      indiv = individualCentrality[key][measure] 
 
-      for key1 in x:
-        change[key1] = abs(x[key1]-G_Cent[item][key1]) #finds absolute value of centrality measure's change
+      for index in indiv:
+        change[index] = abs(x[index]-G_Cent[measure][index]) #finds absolute value of centrality measure's change
 
       mean = 0   
-      for k in change:
-        mean += change[k]
+      for index in change:
+        mean += change[index]
       mean =  mean/int(len(change)) #finds the mean amount each value changed
 
-      if item == 0:
+      if measure == 0:
         full_ls.append([key,"degree",change,mean])
-      if item == 1:
+      elif measure == 1:
         full_ls.append([key,"close",change,mean])
-      if item == 2:
+      elif measure == 2:
         full_ls.append([key,"between",change,mean])
        
-  for item in full_ls:
-    if item[1] == "degree":
-      item[3] = (item[3]-per[0]/per[0]) * 100 #percent change formula 
-    if item[1] == "close":
-      item[3] = (item[3]-per[1]/per[1]) * 100
-    if item[1] == "between":
-      item[3] = (item[3]-per[2]/per[2]) * 100
+  for measure in influentialNodeList:
+    if measure[1] == "degree":
+      measure[3] = (measure[3]-baseline[0]/baseline[0]) * 100 #percent change formula 
+    elif measure[1] == "close":
+      measure[3] = (measure[3]-baseline[1]/baseline[1]) * 100
+    elif measure[1] == "between":
+      measure[3] = (measure[3]-baseline[2]/baseline[2]) * 100
 
-  full_ls = sorted(full_ls, key=itemgetter(3)) #sorts items from smallest mean change to biggest
-  for item in full_ls:
-    print item
-    print 
-    print
+  influentialNodeList = sorted(influentialNodeList, key=itemgetter(3)) #sorts items from smallest mean change to biggest
+  for node in influentialNodeList:
+    print node + "\n \n"
 
-
-  return 
-
+  return
 
 #this function was added to compute the mean of each baseline centrality measure
 #input: a list of dictonaries with centrality measures as values and nodes as keys
 #output: a list of each dictionary's mean
-def baselineCal(lst):
-  ls = []
-  for item in lst: 
-    t = 0
-    for key in item:
-      t += item[key]
-    ls.append(t/(int(len(item))))
-  return ls
-
-
-
-
-
-
-
-
-
+def baselineCal(centralityMeasures):
+  baselines = []
+  for measure in centralityMeasures: 
+    sum = 0
+    for key in measure:
+      sum += measure[key]
+    baseline.append(sum/(int(len(measure))))
+  return baseline
